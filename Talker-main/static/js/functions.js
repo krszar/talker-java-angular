7e39d17fea1f8f64e0a4b8a19e13db292274064f
krszar <65175690+krszar@users.noreply.github.com>
2023-02-04 20:05:25 +0100

    Delete Talker-main/static directory

--- Talker-main/static/js/functions.js
+++
@@ -1,7 +0,0 @@
-function signIn(e) {
-    //e.preventDefault();
-    var login = document.getElementById("login");
-    var password = document.getElementById("password");
-
-    socket.emit("login", {login: login.value, password: password.value});
-}
